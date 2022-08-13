234e064968db90eef4de189f11ccf5ef1b1d0868
Ikuti <m.j.olkiewicz@gmail.com>
2022-08-13 20:15:20 +0200

    Folder/dependecy/scripts setup. Setup player idle

--- Game/Godot-Simple-2D-Platformer/src/Main/Game.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-onready var player: KinematicBody2D = $Player
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("debug_restart"):
-		get_tree().reload_current_scene()
-	if event.is_action_pressed("debug_die"):
-		player.state_machine.transition_to("Die")
