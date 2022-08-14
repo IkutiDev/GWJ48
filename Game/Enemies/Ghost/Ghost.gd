932b9e2dfe25a2ba4c0f80cc77a53575dcd89b18
Ikuti <m.j.olkiewicz@gmail.com>
2022-08-14 20:16:38 +0200

    Improved ghost visuals

--- Game/Enemies/Ghost/Ghost.gd
+++ Game/Enemies/Ghost/Ghost.gd
@@ -23,15 +23,37 @@ var attackMode = false
 
 var cooldown = 0.0
 
+var spawning = true
+
+
+func _on_AnimatedSpriteDeath_finished() -> void:
+	get_tree().get_nodes_in_group("Spawner")[0].record_death(self)
+	animated_sprite.disconnect("animation_finished", self, "_on_AnimatedSpriteDeath_finished")
+	queue_free()
+
+func _on_AnimatedSpriteHit_finished() -> void:
+	animated_sprite.disconnect("animation_finished", self, "_on_AnimatedSpriteHit_finished")
+	animated_sprite.play("float")
+	
+func _on_AnimatedSpriteSpawn_finished() -> void:
+	animated_sprite.disconnect("animation_finished", self, "_on_AnimatedSpriteSpawn_finished")
+	animated_sprite.play("float")
+	spawning = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	spawning = true
 	if get_tree().get_nodes_in_group("Player").size()>0:
 		target = get_tree().get_nodes_in_group("Player")[0]
 	
+	animated_sprite.play("spawn")
+	animated_sprite.connect("animation_finished", self, "_on_AnimatedSpriteSpawn_finished")
 	hitbox.current_health = health
 	pass # Replace with function body.
 
 func fire_bullet():
+	if spawning:
+		return
 	var newBullet = bulletScene.instance()
 	newBullet.global_position = global_position
 	var attackDir = target.global_position - global_position
@@ -43,6 +65,8 @@ func fire_bullet():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	if spawning:
+		return
 	if target == null :
 		desiredLoc = get_global_mouse_position()
 	else:
@@ -68,16 +92,16 @@ func _on_AttackRange_body_exited(body):
 
 
 func _on_Hitbox_got_hit(damage) -> void:
+	if spawning:
+		return
 	# (#Ikuti) Add stagger/push code here, maybe
 	hitbox.current_health -= damage
-
-func _on_AnimatedSprite_finished() -> void:
-	get_tree().get_nodes_in_group("Spawner")[0].record_death(self)
-	animated_sprite.disconnect("animation_finished", self, "_on_AnimatedSprite_finished")
-	queue_free()
+	if hitbox.current_health > 0:
+		animated_sprite.play("hit")
+		animated_sprite.connect("animation_finished", self, "_on_AnimatedSpriteHit_finished")
 
 func _on_Hitbox_died() -> void:
 	# (#Ikuti) Add gain score here
 	# (#Ikuti) Add drop exp/stuff here
 	animated_sprite.play("death")
-	animated_sprite.connect("animation_finished", self, "_on_AnimatedSprite_finished")
+	animated_sprite.connect("animation_finished", self, "_on_AnimatedSpriteDeath_finished")
