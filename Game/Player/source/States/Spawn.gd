600fbf34ffc4a639d5f46fe943a95de2021245f0
Ikuti <m.j.olkiewicz@gmail.com>
2022-08-15 01:56:30 +0200

    Fixed falling on spawn

--- Game/Player/source/States/Spawn.gd
+++ Game/Player/source/States/Spawn.gd
@@ -6,15 +6,16 @@ func _ready() -> void:
 	yield(owner, "ready")
 	_start_position = owner.position
 
+
 func _on_Skin_animation_finished() -> void:
 	_state_machine.transition_to("Move/Idle")
 
 func enter(_msg: Dictionary = {}) -> void:
-	owner.is_active = false
 	owner.position = _start_position
+	# To force is_on_floor in spawn state, to not have falling state right after spawn
+	owner.move_and_slide(Vector2.DOWN, owner.FLOOR_NORMAL)
 	owner.skin.play_animated_sprite("spawn")
 	owner.skin.connect("animated_sprite_finished", self, "_on_Skin_animation_finished")
 
 func exit() -> void:
-	owner.is_active = true
 	owner.skin.disconnect("animated_sprite_finished", self, "_on_Skin_animation_finished")
