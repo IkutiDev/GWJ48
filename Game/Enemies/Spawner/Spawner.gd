720c8e62928ee0dae46b30ce77e1721f94a3330f
Ktosox <ktoso3@gmail.com>
2022-08-14 19:31:22 +0200

    finished up wave spawning

--- Game/Enemies/Spawner/Spawner.gd
+++ Game/Enemies/Spawner/Spawner.gd
@@ -10,25 +10,42 @@ var allGates = []
 
 var allEnemies = []
 
+var waveCounter = 0
+
+var enemiesToSpawn = 0
+
+var enemiesToKill = 100
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	allExits = $Exits.get_children()
-	print(allExits)
 	for z in allExits:
 		if !z.antialiased:
 			allGates.push_back(z)
-	print(allGates)
 	spawn_enemy(enemyData.new())
-	pass # Replace with function body.
+
+func _process(delta):
+	if $WaveTimer.time_left > 0:
+		$Overlay/Label.text = String($WaveTimer.time_left)
+	else:
+		$Overlay/Label.text = ""
 
 func record_death(enemy):
 	allEnemies.erase(enemy)
-	if allEnemies.size()<1:
-		print("wave complete")
+	enemiesToKill -= 1
+	if enemiesToKill < 1:
+		$WaveTimer.start()
+
+func start_next_wave():
+	waveCounter += 1
+	enemiesToSpawn = 5 + waveCounter
+	enemiesToKill = enemiesToSpawn
+	$SpawnTimer.start()
+	pass
 
 func spawn_enemy(data : enemyData):
 	var newEnemy = enemyScenes[data.type].instance()
-	data.walker = true
 	var validExits = []
 	if data.walker:
 		validExits = allGates.duplicate()
@@ -42,8 +59,23 @@ func spawn_enemy(data : enemyData):
 	allEnemies.push_back(newEnemy)
 	pass
 
-func is_a_closer(a,b):
+func is_a_closer(a,b): # is a not a closer! is a me! Mario!
 	var targetPos = $Exits.get_global_mouse_position()
 	return targetPos.distance_to(a.global_position) < targetPos.distance_to(b.global_position)
 	pass
+
+
+
+func _on_SpawnTimer_timeout():
+	spawn_enemy(enemyData.new())
+	enemiesToSpawn -= 1
+	if enemiesToSpawn < 1:
+		$SpawnTimer.stop()
+	pass # Replace with function body.
+
+
+func _on_WaveTimer_timeout():
+	start_next_wave()
+	pass # Replace with function body.
+
 
