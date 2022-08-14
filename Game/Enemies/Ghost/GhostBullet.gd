74000e0d832fa4b425f693a377df60f09786aa3c
Ktosox <ktoso3@gmail.com>
2022-08-14 15:30:45 +0200

    changed how ghost moves to use the Steering singleton

--- Game/Enemies/Ghost/GhostBullet.gd
+++ Game/Enemies/Ghost/GhostBullet.gd
@@ -7,8 +7,6 @@ func _ready():
 	$Explosion.rotation = randf()*2*PI
 
 func _on_GhostBullet_body_entered(body : PhysicsBody2D):
-	if body.has_method("damage"):
-		body.damage(damage)
 	$PopAnimator.play("Pop")
 	linear_velocity = Vector2(0,0)
 	pass # Replace with function body.
