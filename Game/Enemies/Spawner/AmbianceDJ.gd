0ca24b3d185d273f6fb2f53a90307470e7cf5ff9
Ktosox <ktoso3@gmail.com>
2022-08-21 00:29:09 +0200

    more Ambiance + lulaby to SleepScreen

--- Game/Enemies/Spawner/AmbianceDJ.gd
+++ Game/Enemies/Spawner/AmbianceDJ.gd
@@ -7,7 +7,7 @@ func _ready():
 	$EffectTimer.start(8+randi()%7)
 	_on_Impact_finished()
 	_on_Hoot_finished()
-	
+	_on_Plop_finished()
 
 func _on_Hoot_finished():
 	$Hoot.pitch_scale = 0.85 + randf() * 0.15
@@ -16,12 +16,21 @@ func _on_Hoot_finished():
 	pass # Replace with function body.
 
 func _on_Impact_finished():
+	
+	$Impact.pitch_scale = 0.75 + randf() * 0.9
+	pass # Replace with function body.
+
+
+func _on_Plop_finished():
 	$Impact.pitch_scale = 0.75 + randf() * 0.9
 	pass # Replace with function body.
 
 func _on_EffectTimer_timeout():
 	if inBattle:
-		$Impact.play()
+		if randi()%2 == 0:
+			$Impact.play()
+		else:
+			$Plop.play()
 	else:
 		$Hoot.play()
 	$EffectTimer.start(8+randi()%7)
@@ -34,3 +43,6 @@ func _on_EffectTimer_timeout():
 func _on_Background_finished():
 	$Background.play()
 	pass # Replace with function body.
+
+
+
