59cea929c4c10c42e47bc7280300f0f8864b53ba
Ktosox <ktoso3@gmail.com>
2022-08-21 17:35:25 +0200

    made sky turn sligthly red on boss start

--- Game/Boss/BossSpawner.gd
+++ Game/Boss/BossSpawner.gd
@@ -4,10 +4,12 @@ var bossScene = preload("res://Boss/Boss.tscn")
 
 func _on_BossSpawner_body_entered(body):
 	if get_tree().get_nodes_in_group("Spawner")[0].bossReady:
+		var sky = get_tree().get_nodes_in_group("Sky")[0]
 		var oldMoon = get_tree().get_nodes_in_group("Moon")
 		assert(oldMoon.size()>0)
 		oldMoon = oldMoon[0]
 		oldMoon.visible = false
+		sky.modulate = Color(1, 0.384314, 0.384314)
 		var newMoon = bossScene.instance()
 		newMoon.global_position = get_tree().get_nodes_in_group("Camera")[0].get_camera_position() + Vector2(-80,-45)
 		get_parent().get_parent().add_child(newMoon)
